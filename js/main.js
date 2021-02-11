daa13815790281caa2ecc7e1d43e13227cd66f68
artem <platonovartem2005@outlook.com>
2021-02-11 23:45:09 +0300

    load response func

--- js/main.js
+++ js/main.js
@@ -6,16 +6,43 @@ window.onload = () => {
     InitializeDropdowns();
     from.onsubmit = (e) => {
         e.preventDefault();
-        const formData = new FormData(from);
-        formData.set('archiveFile', file);
+        const formData = new FormData();
+        formData.set('template_zip', file);
+
+        let store = {};
+        let checkboxes = document.querySelectorAll('input[type="checkbox"]');
+        checkboxes.forEach(checkbox => {
+            store[`${checkbox.name}`] = checkbox.checked;
+        });
+        let textInputs = document.querySelectorAll('input[type="text"]');
+        textInputs.forEach(inp => {
+            store[`${inp.name}`] = inp.value;
+        });
+        let textAreas = document.querySelectorAll('textarea');
+        textAreas.forEach(textarea => {
+            store[`${textarea.name}`] = textarea.value;
+        });
+
+        formData.set('params', JSON.stringify(store));
+        
+        let watermarkInp = document.getElementById('water-mark-file');
+        formData.set('img_watermark', watermarkInp.files[0]);
+
         sendData(formData);
     }
 
     const sendData = async (data) => {
-        const response = await fetch('/', {
+        const response = await fetch('https://slpv.foxcpp.dev/landing', {
             method: 'POST', 
             body: data
         });
-        console.log(response);
+        const blob = await response.blob();
+        let url = window.URL.createObjectURL(blob);
+        var a = document.createElement('a');
+        a.href = url;
+        a.download = "response.zip";
+        document.body.appendChild(a); // we need to append the element to the dom -> otherwise it will not work in firefox
+        a.click();    
+        a.remove();  //afterwards we remove the element again         
     }
 }
